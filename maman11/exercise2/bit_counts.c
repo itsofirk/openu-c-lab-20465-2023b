652177dce70f522217eca0a32375c564302452ff
Tetro <itsofirk@gmail.com>
2023-03-16 09:20:29 +0200

    bit counts- done - needs check

--- maman11/exercise2/bit_counts.c
+++ maman11/exercise2/bit_counts.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
-#define LONG_SIZE sizeof(unsigned long) * 8
+#define ULONG_SIZE 64
 
-void print_binary(unsigned long num);
+unsigned long read_binary();
 int count_bits(unsigned long num);
 
 int main()
 {
-    unsigned long num = 0b10010100;
-    int res;
-    print_binary(num);
-    res = count_bits(num);
-    printf("\nresult: %d", res);
+	unsigned long num;
+	printf("Hello! This program counts the number of active bits in even places.\n");
+	printf("Please Enter a binary number!: ");
+	num = read_binary();
+
+    printf("\nresult: %ld", num);
     return 0;
 }
 
-void print_binary(unsigned long num) {
-  int i;
-  /* Loop through each bit of the number and print it as 0 or 1*/
-  for (i = LONG_SIZE - 1; i >= 0; i--) {
-    printf("%ld", (num >> i) & 1);
-  }
-  printf("\n");
+unsigned long read_binary(){
+	int i;
+	char num_s[ULONG_SIZE];
+	unsigned long num = 0;
+	scanf("%64[01]", num_s);
+	printf("The number read is %s\n", num_s);
+	for (i = 0; num_s[i]; i++) {
+        num = (num << 1) | (num_s[i] - '0');
+    }
+	return num;
 }
+
 
 int count_bits(unsigned long num){
     int i, sum;
     sum = 0;
-    for(i = LONG_SIZE - 1; i >= 0; i -= 2){
+    for(i = LONG_SIZE - 1; i >= 0; --i){
         if((num >> i) & 1)
             sum++;
+		num >> 1;
     }
     return sum;
 }
